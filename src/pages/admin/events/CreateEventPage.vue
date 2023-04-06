8da2789dfba6569eb5b734d82d2eec4ad97bad6d
David Flek <davidovkyflekovky@gmail.com>
2023-04-06 13:18:42 +0200

    Create event page back online

--- src/pages/admin/events/CreateEventPage.vue
+++ src/pages/admin/events/CreateEventPage.vue
@@ -1,42 +1,11 @@
 <template>
   <div class="row justify-center">
-    <q-card>
-      <q-card-section>
-        <div class="text-h6">Create a new event</div>
-      </q-card-section>
-      <q-card-section class="q-pt-none">
-        <GroupsSelector
-          :groups="eventStore.newEvent.event.groups"
-          @GroupsUpdate="updateGroups"
-        />
-      </q-card-section>
-      <q-card-section>
-        <NewEventForm />
-      </q-card-section>
-
-      <q-card-actions align="right">
-        <q-btn
-          label="Submit"
-          color="primary"
-          @click="eventStore.createEvent()"
-          v-close-popup
-        />
-      </q-card-actions>
-    </q-card>
+    <EventForm :eventId="-1" />
   </div>
 </template>
 
 <script setup lang="ts">
-import GroupsSelector from 'src/components/forms/GroupsSelector.vue';
-import NewEventForm from 'src/components/forms/NewEventForm.vue';
-import { useEventStore } from 'src/stores/event-store';
-import { Event_extended } from 'src/types/dbTypes';
-
-const eventStore = useEventStore();
-
-function updateGroups(groups: Event_extended['groups']) {
-  eventStore.newEvent.event.groups = groups;
-}
+import EventForm from 'src/components/forms/EventForm.vue';
 </script>
 
 <style scoped></style>
