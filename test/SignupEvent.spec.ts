ab03a85ecfc6b35b72d0b0316aed933e9c397c57
David Flek <davidovkyflekovky@gmail.com>
2023-04-12 15:44:31 +0200

    test scripts

--- test/SignupEvent.spec.ts
+++ test/SignupEvent.spec.ts
@@ -8,7 +8,14 @@ test('test', async ({ page }) => {
   await page.getByLabel('Password').fill('heslo');
   await page.getByRole('button', { name: 'Log in' }).click();
 
+  await page.getByText('Choose option').click();
+  await page.getByText('going', { exact: true }).click();
+
   // notify working
   const alertMessage = await page.textContent('[role="alert"]');
   expect(alertMessage).toContain('Action was successful');
+
+  // await page.getByRole('listitem').filter({ hasText: 'Otázka 1 votes: 0 | 0%' }).getByRole('checkbox').click();
+  // // Test that clicked listitem has now text "Otázka 1 votes: 0 | 100%"
+  // await expect(await page.locator('//li[contains(text(), \'Otázka 1 votes: 0 | 100%\')]')).toBeTruthy();
 });
--- test/create-normal-post.spec.ts
+++ test/create-normal-post.spec.ts
@@ -19,20 +19,6 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Text' }).fill('Playwright test Text');
   await page.getByRole('button', { name: 'Submit' }).click();
 
-  // notify working
-  const alertMessage = await page.textContent('[role="alert"]');
-  expect(alertMessage).toContain('Action was successful');
-
   await expect(await page.locator('//div[contains(text(), \'Playwright test heading\')]/following-sibling::div[contains(text(), \'Playwright test Text\')]')).toBeTruthy();
-  await page.getByText('Playwright test heading').click();
-  await page.locator('.q-card__section > button').first().click();
-  await page.getByRole('button').filter({ hasText: 'favorite1' }).click();
-  await page.locator('#f_b37c1278-262e-48e0-9897-f3a9a7157a01').click();
-  await page.locator('#f_b37c1278-262e-48e0-9897-f3a9a7157a01').fill('Random comment');
-  await page.locator('.q-ml-md > .q-btn').first().click();
-  await page.getByText('Action was successful').click();
-  await page.getByRole('button').filter({ hasText: 'comment1' }).click();
-  await page.getByText('Random comment like').click();
-  await page.getByRole('button', { name: 'like' }).click();
-  await page.getByRole('button', { name: 'like' }).click();
+
 });
