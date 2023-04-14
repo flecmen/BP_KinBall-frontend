41848cad741a868120479bb38c162a17153cb5bf
David Flek <davidovkyflekovky@gmail.com>
2023-04-14 10:59:13 +0200

    edit component

---
+++ src/components/feed/post/components/EditPost.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-item class="justify-end q-gutter-sm" dense>
+    <q-icon
+      name="edit"
+      size="xs"
+      color="primary"
+      class="click-cursor"
+      @click="emit('edit-post', post.id)"
+    >
+      <q-tooltip>edit post</q-tooltip>
+    </q-icon>
+    <q-icon name="delete" size="xs" color="red" class="click-cursor">
+      <q-tooltip>delete post</q-tooltip>
+      <q-menu auto-close>
+        <q-card class="my-card">
+          <q-card-section class="bg-primary text-white">
+            <div class="text">Are you sure to remove post permanently?</div>
+          </q-card-section>
+
+          <q-card-actions align="right">
+            <q-btn
+              flat
+              color="white"
+              class="bg-red"
+              @click="postStore.deletePost(props.post.id)"
+              >Delete</q-btn
+            >
+          </q-card-actions>
+        </q-card>
+      </q-menu>
+    </q-icon>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+import { usePostStore } from 'src/stores/post-store';
+import { Post_extended } from 'src/types/dbTypes';
+
+export interface Props {
+  post: Post_extended;
+  isMobile: boolean;
+}
+const props = defineProps<Props>();
+
+const emit = defineEmits<{
+  (event: 'edit-post', postIt: Post_extended['id']): void;
+}>();
+
+const postStore = usePostStore();
+</script>
+
+<style scoped>
+.click-cursor {
+  cursor: pointer;
+}
+</style>
